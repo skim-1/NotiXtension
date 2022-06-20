51e6e2d0c18da586053deda03a3e8590c491d448
sunny <sunny2004.kim@gmail.com>
2022-06-20 14:12:50 -0500

    build frontend jonathan

--- popup.js
+++ popup.js
@@ -1,8 +1,22 @@
 chrome.tabs.executeScript( {
   code: "window.getSelection().toString();"
-}, function(selection) {
-  alert(selection[0]);
-  var chicken = selection[0]
-});
+}, async function(selection) {
+  // alert(selection[0]);
+  var chicken = selection[0];
+  // alert(chicken);
 
-//joe mama
+  await fetch ('http://localhost:5000/spam',  {
+    method: 'POST',
+    body: JSON.stringify({
+      'text': chicken
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    }
+  })
+  .then(async function(res){
+    let d = await res.json();
+    document.write(d.msg);
+  });
+
+});
